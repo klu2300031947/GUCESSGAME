c97950d67abf53980254e1c874859d7620190c37
klu2300031947 <163754507+klu2300031947@users.noreply.github.com>
2025-01-22 10:45:29 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to start the guessing game
+function start_game() {
+  local correct_count=$(ls -1 | wc -l)  # Get the number of files in the current directory
+  local guess=-1
+
+  while [[ $guess -ne $correct_count ]]; do
+    echo "How many files are in the current directory?"
+    read guess
+
+    if [[ $guess -lt $correct_count ]]; then
+      echo "Your guess is too low. Try again."
+    elif [[ $guess -gt $correct_count ]]; then
+      echo "Your guess is too high. Try again."
+    fi
+  done
+
+  echo "Congratulations! You guessed the correct number of files."
+}
+
+# Call the function to start the game
+start_game
